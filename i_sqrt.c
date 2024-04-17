a99bd9ebcca4a08c74a9d8987bc81a1e58e11e32
kevinzxc1217 <kevinzxc1217@gmail.com>
2024-04-17 15:41:28 +0800

    Replace __builtin_clz with ffs and remove branching
    This commit modifies the sqrt function by replacing the __builtin_clz function with ffs. Additionally, it removes branching inside the loop to optimize the calculation process.

--- i_sqrt.c
+++ i_sqrt.c
@@ -7,13 +7,21 @@ int i_sqrt(int x)
 {
     if (x <= 1) /* Assume x is always positive */
         return x;
+    int msb;
+    int tmp = x;
+    while(tmp){
+        tmp = tmp >> ffs(x);
+        msb += ffs(x);
+    }
 
     int z = 0;
-    for (int m = 1UL << ((31 - __builtin_clz(x)) & ~1UL); m; m >>= 2) {
+    for (int m = 1UL << (msb & ~1UL); m; m >>= 2) {
         int b = z + m;
         z >>= 1;
-        if (x >= b)
-            x -= b, z += m;               
+        
+        int mask = -(x >= b);
+        x -= b & mask;
+        z += m & mask;
     }
     return z;
 }
